670c64907f9874c33592ba1c991adb6b84ee1ab6
tony <tony.spicer.covin@gmail.com>
2019-04-18 14:47:35 -0500

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id ="app">
+    <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
+    <StudentTable 
+     v-bind:students="students"
+     v-on:student-present="studentArrivedOrLeft"
+     v-on:delete-student="studentDeleted">
+    </StudentTable>
+    <StudentMessage v-bind:message="message" v-bind:name="name"></StudentMessage>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NewStudentForm from './components/NewStudentForm.vue'
+import StudentTable from './components/StudentTable.vue'
+import StudentMessage from './components/StudentMessage.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      students: [
+        { name: 'example', starID: 'aa1234aa', present: true },
+      ],
+      message: '',
+      name: ''
+    }
+  },
+    components:{
+        NewStudentForm,
+        StudentTable,
+        StudentMessage
+    },
+    methods: {
+    newStudentAdded(student){
+            this.students.push(student)
+            console.log(student)
+            this.students.sort(function(s1,s2){
+                return s1.name.toLowerCase() < s2.name.toUpperCase() ? -1 : 1
+            })
+        },
+    studentArrivedOrLeft(student) {
+      this.message = student.present ? 'Welcome,' : 'Goodbye, '
+      this.name = student.name  
+    },
+    studentDeleted(student) {
+      this.students = this.students.filter( function(s) { return s != student })
+    }
+    }
 }
 </script>
+</style>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
